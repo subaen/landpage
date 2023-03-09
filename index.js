ac135e48488cbf69d74b50c1741998c1b6bc7960
subaen <127117169+subaen@users.noreply.github.com>
2023-03-09 15:08:34 +0700

    as

--- index.js
+++ index.js
@@ -1,39 +1,31 @@
-onscroll = () =>{
-
-    if(scrollY > 60 ){
-        document.querySelector('.navbar').classList.add('active');
-    }
-    else{
-        document.querySelector('.navbar').classList.remove('active');
-    }
-}
-onload = () =>{
-
-    if(scrollY > 60 ){
-        document.querySelector('.navbar').classList.add('active');
-    }
-    else{
-        document.querySelector('.navbar').classList.remove('active');
-    }
+window.onscroll = () =>{
+  if (window.scrollY > 40) {
+    document.querySelector('header-2').classList.add('active');
+  }
 }
 
 
 var swiper = new Swiper(".books-slider", {
+  spaceBetween:10,
    loop:true,
    centeredSlides:true,
    autoplay:{
     delay: 9500,
     disableoninteraction: false,
    },
+   navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
     breakpoints: {
       "0": {
         slidesPerView: 1,
       },
       "768": {
-        slidesPerView: 3,
+        slidesPerView: 2,
       },
       "1024": {
-        slidesPerView: 4,
+        slidesPerView: 3,
       },
     },
   });
@@ -63,6 +55,12 @@ var swiper = new Swiper(".books-slider", {
        1024: {
          slidesPerView: 4,
        },
+       2048: {
+        slidesPerView: 5,
+      },
+       4096: {
+        slidesPerView: 6,
+      }
      },
    });
 
