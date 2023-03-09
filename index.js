5616d93d0de8e4340ca7b27166410feb7ad29647
HuyJun2305 <125681217+HuyJun2305@users.noreply.github.com>
2023-03-09 11:46:34 +0700

    phan xu huong

--- index.js
+++ index.js
@@ -37,3 +37,34 @@ var swiper = new Swiper(".books-slider", {
       },
     },
   });
+
+  var swiper = new Swiper(".xu-huong-slider", {
+    spaceBetween:10,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+     delay: 9500,
+     disableoninteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+     breakpoints: {
+       0: {
+         slidesPerView: 1,
+       },
+       450: {
+        slidesPerView: 2,
+      },
+       768: {
+         slidesPerView: 3,
+       },
+       1024: {
+         slidesPerView: 4,
+       },
+     },
+   });
+
+
+ 
