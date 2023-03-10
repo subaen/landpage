26390285efa9d0f66f7d3926d643f2ff04b26811
HuyJun2305 <125681217+HuyJun2305@users.noreply.github.com>
2023-03-10 20:31:22 +0700

    upload

--- index.js
+++ index.js
@@ -121,3 +121,25 @@ var swiper = new Swiper(".books-slider", {
      },
    });
  
+   var swiper = new Swiper(".blogs-slider", {
+    spaceBetween:10,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+     delay: 9500,
+     disableoninteraction: false,
+    },
+
+     breakpoints: {
+       0: {
+         slidesPerView: 1,
+       },
+       
+       768: {
+         slidesPerView: 2,
+       },
+       1024: {
+         slidesPerView: 3,
+       },
+     },
+   });
